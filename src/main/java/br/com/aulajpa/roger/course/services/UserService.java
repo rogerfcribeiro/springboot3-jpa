33512fd01c5ee14d860de4ff992933b3a9fe7b5d
Roger Francis Cardoso Ribeiro <rogerfcribeiro@gmail.com>
2023-01-11 18:59:31 -0400

    Exception handling - update

--- src/main/java/br/com/aulajpa/roger/course/services/UserService.java
+++ src/main/java/br/com/aulajpa/roger/course/services/UserService.java
@@ -12,6 +12,7 @@ import br.com.aulajpa.roger.course.entities.User;
 import br.com.aulajpa.roger.course.repositories.UserRepository;
 import br.com.aulajpa.roger.course.services.exceptions.DatabaseException;
 import br.com.aulajpa.roger.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -45,9 +46,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity,obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity,obj);
+			return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
