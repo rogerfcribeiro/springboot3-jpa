e00a71ca1e83ace8d9e5c07c3ed5e7d319389d6c
Roger Francis Cardoso Ribeiro <rogerfcribeiro@gmail.com>
2023-01-11 18:31:25 -0400

    Exception handling - delete

--- src/main/java/br/com/aulajpa/roger/course/services/UserService.java
+++ src/main/java/br/com/aulajpa/roger/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.com.aulajpa.roger.course.entities.User;
 import br.com.aulajpa.roger.course.repositories.UserRepository;
+import br.com.aulajpa.roger.course.services.exceptions.DatabaseException;
 import br.com.aulajpa.roger.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException ex) {
+			throw new DatabaseException(ex.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
