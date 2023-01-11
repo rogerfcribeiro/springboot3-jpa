eb4395e52accedc1ee9cbe55b11342ce5ac27953
Roger Francis Cardoso Ribeiro <rogerfcribeiro@gmail.com>
2023-01-11 18:14:29 -0400

    Exception handling - findById

--- src/main/java/br/com/aulajpa/roger/course/services/UserService.java
+++ src/main/java/br/com/aulajpa/roger/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.aulajpa.roger.course.entities.User;
 import br.com.aulajpa.roger.course.repositories.UserRepository;
+import br.com.aulajpa.roger.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
